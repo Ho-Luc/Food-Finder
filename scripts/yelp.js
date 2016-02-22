73b0073167733221e8f215c4ba8fd2ddf50e0dca
icarrera <irismcarrera@gmail.com>
2016-02-22 10:05:59 -0800

    created file that gets the yelp query specified. may change later.

--- scripts/yelp.js
+++ scripts/yelp.js
@@ -1,13 +1,20 @@
-// <-- placeholder file for dir init to work-->
+(function(module){
+  var foodBanks = {};
+  foodBanks.all = [];
 
-// (function(module){
-//   var yelp = {};
-//
-//   yelp.ajax = function(callback) {
-//     $.ajax('/v2/search', {
-//
-//     })
-//   }
-//
-//   module.yelp = yelp;
-// })(window);
+  foodBanks.requestYelp = function(callback) {
+    $.get('https://api.yelp.com/v2/search/?location=Seattle, WA&category_filter=foodbanks')
+      .done(function(data, message, xhr) {
+        foodBanks.all = data;
+      }).done(callback);
+  };
+
+  foodBanks.with = function(attr) {
+    return foodBanks.all.filter(function(foodbank) {
+      return foodbank[attr];
+    });
+  };
+
+  module.foodBanks = foodBanks;
+
+})(window);
