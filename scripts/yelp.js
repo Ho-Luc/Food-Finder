2a2d9850a0a1b5746599c04d62b630b3e1fadbe4
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-21 18:34:48 -0800

    last set of files that weren't added in first commit, for set up

---
+++ scripts/yelp.js
@@ -0,0 +1,13 @@
+// <-- placeholder file for dir init to work-->
+
+// (function(module){
+//   var yelp = {};
+//
+//   yelp.ajax = function(callback) {
+//     $.ajax('/v2/search', {
+//
+//     })
+//   }
+//
+//   module.yelp = yelp;
+// })(window);
