21e22e179f11a2bb4aeab4d5e96a9b72ada6997c
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-22 09:45:44 -0800

    need to switch to master

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,15 @@
-var initMap = function() {
-  var map;
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.6097, lng: -122.3331},
-    zoom: 11
-  });
-  console.log('map ' + map);
-};
+(function(module) {
+  var googleMap = {};
+  // var myLatLng = new google.maps.LatLng(yelpLat, yelpLng);
+
+  googleMap.initMap = function() {
+    var map;
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 47.6097, lng: -122.3331},
+      zoom: 11
+    });
+  };
+
+
+  module.googleMap = googleMap;
+})(window);
