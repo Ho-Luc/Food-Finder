fc39ab16fd2fe0219c7e2d6e486cfa2b38b549c9
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-02-22 13:09:02 -0800

    re-added package.json based on Iris's feature_yelp_api branch. Added skeletal mealView.js file for select-menu purposes one we've filtered more

---
+++ scripts/mealView.js
@@ -0,0 +1,43 @@
+(function(module){
+  var mealView = {};
+
+  var render = function(article){
+  // handlebar template script
+  }
+
+
+ mealView.populateFilters = function(){
+/// populate separate dropdowns with meal types, demographic(s) served, and time
+
+mealView.allMeals(function(rows){
+  // append all meal types (lunch, dinner, etc) to dropDown menu filter.
+}
+
+mealView.allPeople(function(rows){
+  // append all demographics served to dropDown menu filter.
+}
+
+mealView.allTimes(function(rows){
+  // append all available hours to dropDown menu filter.
+}
+
+}
+
+mealView.handleFilters = function(){
+// handling the events.
+
+  }
+
+  mealView.index = function(articles){
+    //initiate page/dom queries and call populateFilters and handleFilters;
+
+    // mealView.populateFilters();
+    // mealView.handleFilters();
+
+  }
+}
+
+module.mealView = mealView;
+
+
+})(window);
