b51bea76ed9341599a65d815becc1584cbc33d70
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-23 10:46:02 -0800

    currently dropping yelp api for geocoding to convert addresses to lat-long for maps

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,15 +1,21 @@
 (function(module) {
   var googleMap = {};
-  // var myLatLng = new google.maps.LatLng(yelpLat, yelpLng);
+  var map;
+  // var myLatLng = {lat: -25.363, lng: 131.044};
 
   googleMap.initMap = function() {
-    var map;
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 47.6097, lng: -122.3331},
       zoom: 11
     });
   };
 
+  googleMap.requestGeoloction = function(callback) {
+    $.ajax({
+      url: 'https:maps.googleapis.com/maps/api/geocode/json',
+
+    })
+  };
 
   module.googleMap = googleMap;
 })(window);
