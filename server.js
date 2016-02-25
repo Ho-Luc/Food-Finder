99b7a91e570dcb6638d395ab90da1d0a323056b8
icarrera <irismcarrera@gmail.com>
2016-02-25 15:41:14 -0800

    removed yelp stuff

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-//require() is node.js's module loading system
 var express = require('express');
-//load url and this variable is used below
-// var url = require('url');
-// var fs = require('fs');
-//inintializes express app
+
 var app = express();
-// var yelpJS = require(__dirname + '/scripts/yelp.js');
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
