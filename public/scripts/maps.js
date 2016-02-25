4f99a1e479ca136a34981c1f9736f69804b40dc9
lwenke01 <lwenke@gmail.com>
2016-02-25 15:12:48 -0800

    updated json data -cleaned

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -4,11 +4,13 @@
   var markers = [];
   // var addressArray = ['4201 Letitia Ave S', '511 Boren Ave N, Seattle, WA 98109', '400 Broad St, Seattle, WA 98109']; //place holder array until we get JSON addresses
   var map;
+  var infoWindow = new google.maps.InfoWindow();
+  var latlngbounds = new google.maps.LatLngBounds();
 
   googleMap.initMap = function() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 47.6097, lng: -122.3331},
-      zoom: 11
+      zoom: 13
     });
 
   };
@@ -19,12 +21,22 @@
       var marker = new google.maps.Marker({
         position: val.results[0].geometry.location,
         map: map,
-        title: mealView.filteredData.programName
+        title: mealView.filteredData[idx].programName
       });
       console.log(val.results[0].geometry.location);
 
-    });
-  };
+    });(function (marker, data) {
+        var infoWindow = new google.maps.InfoWindow();
+                google.maps.event.addListener(marker, "click", function (e) {
+                    infoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.title + 'here' + "</div>");
+                    infoWindow.open(map, marker);
+                });
+            })(marker, data);
+            latlngbounds.extend(marker.position);
+  }
+
+
+
 
   //this function grabs an array of addresses and turns them into geocoordinates. LIMIT 10 PER REQUEST!
   googleMap.requestGeocoding = function(callback) {
