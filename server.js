c56f950082293cd7eb69f6da52d5df4082cf2b33
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-25 16:42:56 -0800

    fiexed server.js file error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
 //
