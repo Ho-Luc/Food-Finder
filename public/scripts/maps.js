19c34563e9de75c3f8dd76fd08cb5652a5cff059
YOUR FULL NAME <Ho.luc13@gmail.com>
2016-02-25 17:59:01 -0800

    need to switch branches

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -30,6 +30,33 @@
     });
   };
 
+  // googleMap.getLatLong = function(address) {
+  //   var geocoder = new google.maps.Geocoder();
+  //   geocoder.geocode( {'address': address}, function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       latLngArray.push(results[0].geometry.location);
+  //       console.log(address);
+  //       console.log(results[0].geometry.location);
+  //     } else {
+  //       result = "Unable to find address: " + status;
+  //       console.log('did not work');
+  //     }
+  //   });
+  //   console.log(latLngArray);
+  // }
+  googleMap.getLatLong = function(address) {
+    var geocoder = new google.maps.Geocoder();
+    var result = "";
+    geocoder.geocode( { 'address': address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        result = results[0].geometry.location;
+      } else {
+        result = "Unable to find address: " + status;
+      }
+    });
+    return result;
+  }
+
   //this function grabs an array of addresses and turns them into geocoordinates. LIMIT 10 PER REQUEST!
   googleMap.requestGeocoding = function(callback) {
     latLngArray.length = 0;
